59419ad8665904e354626877546d297fe3f36408
Manny-14 <okorieemmanuelapp@gmail.com>
2024-12-01 10:46:04 -0600

    still attempting to solve sign in bug

--- backend/controller/user/userDetails.js
+++ backend/controller/user/userDetails.js
@@ -2,6 +2,7 @@ const userModel = require('../../models/userModel')
 
 async function userDetailsController(req, res) {
     try {
+        console.log("Where is request.userID", req.userId)
         const user = await userModel.findById(req.userId)
 
         res.status(200).json({
