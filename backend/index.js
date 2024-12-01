b1beb6218a81f93c8caeeff1f5948cec1204ba9f
Manny-14 <okorieemmanuelapp@gmail.com>
2024-12-01 08:23:52 -0600

    still working on vercel

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "https://fiskhub.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+    // res.header("Access-Control-Allow-Credentials", "true");
     if (req.method === "OPTIONS") {
         return res.sendStatus(200);
     }
